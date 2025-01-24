e4adb0a4591b5bec231ddfec83f10bfaeb7d5083
Sivakumar M <154248666+msivakumarv@users.noreply.github.com>
2025-01-24 20:43:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-   @returns {JSX.Element}
 
 function App() {
   return (
     <Router>
       <Switch>
-        {/* Route for the home page */}
         <Route exact path="/" component={Home} />
-
-        {/* Route for the about page */}
         <Route path="/about" component={About} />
-
-        {/* Redirect all other paths to home */}
         <Redirect to="/" />
       </Switch>
     </Router>
