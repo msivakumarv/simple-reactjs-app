393471a8482b6af90b738b398aca2812736f310d
Sivakumar M <154248666+msivakumarv@users.noreply.github.com>
2025-01-24 20:55:11 +0530

    Update Customers.js

--- src/Customers.js
+++ src/Customers.js
@@ -1,58 +1,73 @@
-import React, {Component} from 'react';
-import Panel from 'react-bootstrap/lib/Panel'
-import Button from 'react-bootstrap/lib/Button'
-import CustomerDetails from './CustomerDetails'
-import axios from 'axios'
+import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import CustomerDetails from './CustomerDetails';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 
 export default class Customers extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      selectedCustomer: 1
-    }
+      selectedCustomer: null,
+      customerList: null,
+      error: null,
+    };
   }
 
-  //function which is called the first time the component loads
   componentDidMount() {
     this.getCustomerData();
   }
 
-  //Function to get the Customer Data from json
+  // Function to fetch customer data from JSON
   getCustomerData() {
-    axios.get('assets/samplejson/customerlist.json').then(response => {
-      this.setState({customerList: response})
-    })
-  };
-
-  render() {
-    if (!this.state.customerList)
-      return (<p>Loading data</p>)
-    return (<div className="addmargin">
-      <div className="col-md-3">
-        {
-
-          this.state.customerList.data.map(customer => <Panel bsStyle="info" key={customer.name} className="centeralign">
-            <Panel.Heading>
-              <Panel.Title componentClass="h3">{customer.name}</Panel.Title>
-            </Panel.Heading>
-            <Panel.Body>
-              <p>{customer.email}</p>
-              <p>{customer.phone}</p>
-              <Button bsStyle="info" onClick={() => this.setState({selectedCustomer: customer.id})}>
-
-                Click to View Details
-
-              </Button>
-
-            </Panel.Body>
-          </Panel>)
-        }
-      </div>
-      <div className="col-md-6">
-        <CustomerDetails val={this.state.selectedCustomer}/>
-      </div>
-    </div>)
+    axios
+      .get('assets/samplejson/customerlist.json')
+      .then((response) => {
+        this.setState({ customerList: response.data });
+      })
+      .catch((error) => {
+        console.error('Error fetching customer data:', error);
+        this.setState({ error: 'Failed to load customer data' });
+      });
   }
 
+  render() {
+    const { customerList, selectedCustomer, error } = this.state;
+
+    if (error) return <p className="text-danger">{error}</p>;
+
+    if (!customerList) return <p>Loading data...</p>;
+
+    return (
+      <div className="addmargin">
+        <div className="col-md-3">
+          {customerList.map((customer) => (
+            <Card key={customer.id} className="mb-3 text-center">
+              <Card.Header as="h5">{customer.name}</Card.Header>
+              <Card.Body>
+                <Card.Text>Email: {customer.email}</Card.Text>
+                <Card.Text>Phone: {customer.phone}</Card.Text>
+                <Button
+                  variant="info"
+                  onClick={() => this.setState({ selectedCustomer: customer.id })}
+                >
+                  View Details
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+        <div className="col-md-6">
+          {selectedCustomer && <CustomerDetails val={selectedCustomer} />}
+        </div>
+      </div>
+    );
+  }
 }
+
+// PropTypes validation
+Customers.propTypes = {
+  selectedCustomer: PropTypes.number,
+  customerList: PropTypes.array,
+  error: PropTypes.string,
+};
