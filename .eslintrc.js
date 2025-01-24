f56440608c4560dda5b875012c9fca4b540ca2be
Sivakumar M <154248666+msivakumarv@users.noreply.github.com>
2025-01-24 20:58:35 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
     'es6': true,
     'node': true,
   },
-  'extends': 'google',
+  'extends': [
+    'google',
+    'plugin:react/recommended', // React-specific linting rules
+  ],
   'globals': {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly',
@@ -20,5 +23,14 @@ module.exports = {
     'react',
   ],
   'rules': {
+    'react/prop-types': 'warn', // Warn when prop types are not defined
+    'react/jsx-uses-react': 'error', // Prevent unused React import in JSX files
+    'react/jsx-uses-vars': 'error', // Prevent unused variables in JSX files
+    'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }], // Allow unused variables prefixed with _ (common for unused function args)
+    'max-len': ['error', { 'code': 100 }], // Enforce a maximum line length of 100 characters
+    'object-curly-spacing': ['error', 'always'], // Ensure spacing inside curly braces
+    'comma-dangle': ['error', 'always-multiline'], // Ensure consistent use of trailing commas
+    'indent': ['error', 2], // Enforce 2 spaces indentation
+    'no-console': 'warn', // Warn on console.log usage
   },
 };
